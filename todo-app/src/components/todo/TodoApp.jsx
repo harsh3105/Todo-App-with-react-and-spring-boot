8ce43a16c8ecf0b978b2af0cfc16fe07dbf7d3d1
harsh3105 <harshgrover3105@gmail.com>
2020-05-03 21:34:28 +0530

    Created login frontend

--- todo-app/src/components/todo/TodoApp.jsx
+++ todo-app/src/components/todo/TodoApp.jsx
@@ -18,30 +18,55 @@ class LoginComponent extends Component{
             username : 'harsh',
             password : ''
         }
-        this.handlerUsernameChange = this.handlerUsernameChange.bind(this);
-        this.handlerPasswordChange = this.handlerPasswordChange.bind(this);
+
+        // this.handlerUsernameChange = this.handlerUsernameChange.bind(this);
+        // this.handlerPasswordChange = this.handlerPasswordChange.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.loginClicked = this.loginClicked.bind(this);
     }
 
-    handlerUsernameChange(event){
+    handleChange(event){
         this.setState(
             {
-                username: event.target.value
+                [event.target.name] : event.target.value 
             }
         )
     }
-    handlerPasswordChange(event){
-        this.setState(
-            {
-                password: event.target.value
-            }
-        )
+
+    // handlerUsernameChange(event){
+    //     this.setState(
+    //         {
+    //             username: event.target.value
+    //         }
+    //     )
+    // }
+    // handlerPasswordChange(event){
+    //     this.setState(
+    //         {
+    //             password: event.target.value
+    //         }
+    //     )
+    // }
+
+    loginClicked(){
+        //harsh,harsh
+        if(this.state.username==="harsh" && this.state.password==="harsh"){
+            console.log("s");
+        }
+        else{
+            console.log("f");
+        }
+        //console.log(this.state);
     }
+
     render(){
         return(
            <div>
-                Username: <input type="text" name="username" value={this.state.username} onChange={this.handlerUsernameChange}/>
-                Password: <input type="password" name="password" value={this.state.password} onChange={this.handlerPasswordChange}/>
-                <button>Login</button>
+               <div>Invalid Credentials</div>
+               <div>Login Successfully</div>
+                Username: <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
+                Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
+                <button onClick={this.loginClicked}>Login</button>
            </div>
         );
     }
